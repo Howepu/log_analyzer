ce050745c017446219842f6208799351132ef23e
Howepu <125370383+Howepu@users.noreply.github.com>
2024-11-02 07:13:22 +0300

    Исправлен checkstyle.

--- src/main/java/backend/academy/log_analyzer/Arguments.java
+++ src/main/java/backend/academy/log_analyzer/Arguments.java
@@ -4,6 +4,5 @@ import java.time.LocalDate;
 import java.util.List;
 
 public record Arguments(List<String> paths, LocalDate from, LocalDate to, String outputFormat,
-                        String filterField, String filterValue) {
-}
+                        String filterField, String filterValue) {}
 
